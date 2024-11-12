386c3e7b195009801fcc9514eb9f762f3348d86a
ClaireCooper <cjc233@cam.ac.uk>
2024-11-12 11:29:58 +0000

    Assess: methods to plot gdf features

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -1,8 +1,9 @@
 from .config import *
 
-import pandas as pd
-
 from . import access
+
+import pandas as pd
+import matplotlib.pyplot as plt
 
 """These are the types of import we might expect in this file
 import pandas
@@ -12,11 +13,16 @@ import matplotlib.pyplot as plt
 import sklearn.decomposition as decomposition
 import sklearn.feature_extraction"""
 
-"""Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
+"""Place commands in this file to assess the data you have downloaded. How are missing values encoded, 
+how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. 
+Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly 
+timezoned."""
 
 
 def data():
-    """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
+    """Load the data from access and ensure missing values are correctly encoded as well as indices correct,
+    column names informative, date and times correctly formatted. Return a structured data structure such as a data
+    frame."""
     df = access.data()
     raise NotImplementedError
 
@@ -45,3 +51,20 @@ def buildings_with_addresses(buildings_df):
 
 def areas_from_gdf(gdf):
     return gdf['geometry'].to_crs(epsg=6933).area
+
+
+def plot_features_on_axis(axis, gdf):
+    gdf.plot(ax=axis, linewidth=1, edgecolor="dimgray")
+
+
+def plot_gdfs(gdfs):
+    plt.rcParams['axes.formatter.useoffset'] = False
+    fig, ax = plt.subplots()
+    for gdf in gdfs:
+        plot_features_on_axis(ax, gdf)
+
+    ax.set_xlabel("longitude")
+    ax.set_ylabel("latitude")
+
+    plt.tight_layout()
+    plt.show()
