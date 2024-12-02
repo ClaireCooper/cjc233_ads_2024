7d65326fe3038df437307a5fa58baff0cc9c62ec
ClaireCooper <cjc233@cam.ac.uk>
2024-12-02 19:46:43 +0000

    Address: Get output area from coordinates

--- fynesse/address.py
+++ fynesse/address.py
@@ -13,6 +13,16 @@ import tensorflow as tf
 
 # Or if it's a statistical analysis
 import scipy.stats"""
+import pandas as pd
+from pyproj import Transformer
 
 """Address a particular question that arises from the data"""
 
+
+def get_output_area_from_coordinates(conn, longitude, latitude):
+    transformer = Transformer.from_crs("EPSG:4326", "EPSG:27700",  always_xy=True)
+    easting, northing = transformer.transform(longitude, latitude)
+    query = (f'SELECT output_area FROM oa_data '
+             f'WHERE ST_CONTAINS(geometry, ST_GeomFromText("POINT({easting} {northing})")) LIMIT 1')
+    df = pd.read_sql(query, conn)
+    return df['output_area']
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "pandas", "numpy", "jupyter", "matplotlib", "pymysql", "kneed", "osmnx<=1.9.4", "osmium"
+    "pandas", "numpy", "jupyter", "matplotlib", "pymysql", "kneed", "osmnx<=1.9.4", "osmium", "statsmodels"
 ]
 
 # What packages are optional?
