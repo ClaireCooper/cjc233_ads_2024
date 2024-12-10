ce93143c00bedbb10992c3cb3121b1508b762f3f
ClaireCooper <cjc233@cam.ac.uk>
2024-12-10 11:15:49 +0000

    Assess: Single feature visualisations, allow single rows and or columns of plots

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -494,24 +494,28 @@ def plot_r2s_bar_chart(r2s):
 
 def plot_single_feature_predictors(rows, cols, train_x, all_y, models, design_fns, title):
     fig, axes = plt.subplots(rows, cols, figsize=(15, 10), sharey=True)
+    if rows > 1 and cols > 1:
+        axes = list(np.concatenate(axes).flat)
     fig.suptitle(title)
     for i, col in enumerate(train_x.columns):
-        axes[i // cols][i % cols].plot(train_x[col], all_y.loc[train_x.index], 'b.')
-        axes[i // cols][i % cols].set_title(col)
-        axes[i // cols][i % cols].set_ylim(2.5, 5)
+        axes[i].plot(train_x[col], all_y.loc[train_x.index], 'b.')
+        axes[i].set_title(col)
+        axes[i].set_ylim(2.5, 5)
         x = np.linspace(0, train_x[col].max(), 100)
         for name, f in design_fns.items():
             y = models[col][name].predict(sm.tools.add_constant(f(x)))
-            axes[i // cols][i % cols].plot(x, y, '-r')
+            axes[i].plot(x, y, '-r')
     plt.show()
 
 
 def plot_single_feature_residuals(rows, cols, test_x, all_y, models, design_fn_name='linear', design_fn=lambda x: x):
     fig, axes = plt.subplots(rows, cols, figsize=(15, 10), sharey=True)
+    if rows > 1 and cols > 1:
+        axes = list(np.concatenate(axes).flat)
     fig.suptitle('Residuals')
     for i, col in enumerate(test_x.columns):
-        axes[i // cols][i % cols].set_title(col)
+        axes[i].set_title(col)
         x = test_x[col]
         y = models[col][design_fn_name].predict(sm.tools.add_constant(design_fn(x)))
-        axes[i // cols][i % cols].plot(x, all_y.loc[test_x.index] - y, 'b.')
+        axes[i].plot(x, all_y.loc[test_x.index] - y, 'b.')
     plt.show()
